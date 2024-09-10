f8849c4f554af10c2d83e2364a6268fc8fd8a53b
DaniM0613 <wwh.rj.92@gmail.com>
2024-09-10 17:02:15 -0600

    first commit

---
+++ app/order/page.tsx
@@ -0,0 +1,10 @@
+
+
+
+export default function OrderPage() {
+  return (
+    <h1>
+      OrderPage
+    </h1>
+  )
+}
---
+++ app/products/page.tsx
@@ -0,0 +1,12 @@
+
+
+
+export default function ProductsPage() {
+
+   
+  return (
+    <div>
+      ProductsPage
+    </div>
+  )
+}
