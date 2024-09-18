ee4eaf66bfe955fe12eb44a08b08701725a8fbe0
DaniM0613 <wwh.rj.92@gmail.com>
2024-09-18 13:43:56 -0600

    Finalizando los card de productos

--- components/products/ProductCard.tsx
+++ components/products/ProductCard.tsx
@@ -1,5 +1,6 @@
 import type { Product } from '@prisma/client'
 import { formatCurrency } from '@/src/utils'
+import Image from 'next/image'
 
 
 type ProductCardProps = {
@@ -7,13 +8,27 @@ type ProductCardProps = {
 }
 export default function ProductCard({product} : ProductCardProps) {
   return (
-    <div  className='border bg-white'>
+    <div  className='border bg-white '>
+
+      <Image
+        width={400}
+        height={500}
+        src={`/products/${product.image}.jpg`}
+        alt={`Imagen patillo ${product.name}`}
+      />
 
         <div className='p-5'>
           <h3 className='text-2xl font-bold'>{product.name}</h3>
           <p className='mt-5 font-black text-4xl text-amber-500'>
             {formatCurrency(product.price)}
           </p>
+          <button 
+             type='button'
+             className='bg-indigo-500 hover:bg-indigo-700 text-white w-full mt-5 p-3 uppercase font-bold cursor-pointer'
+          >
+            Agregar
+          </button>
+        
         </div>
       
     </div>
