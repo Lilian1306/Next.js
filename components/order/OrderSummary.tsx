f62c00c942af1386ae26d001568801c61cc0b8a4
DaniM0613 <wwh.rj.92@gmail.com>
2024-09-25 15:50:34 -0600

    Recuperar datos de formularion con formData

--- components/order/OrderSummary.tsx
+++ components/order/OrderSummary.tsx
@@ -10,8 +10,8 @@ export default function OrderSummary() {
   const order = useStore((state) => state.order)
   const total = useMemo(() => order.reduce((total, item) => total + (item.quantity * item.price), 0) ,[order])
 
-  const handleCreateOrder = () => {
-    console.log('desde handleCreateOrder')
+  const handleCreateOrder = (formData: FormData) => {
+    console.log(formData.get('name'))
 
     createOrder()
   }
@@ -36,6 +36,14 @@ export default function OrderSummary() {
              className="w-full mt-10 space-y-5"
              action={handleCreateOrder}
           >
+
+            <input
+              type= "text"
+              placeholder="Tu Nombre"
+              className= "bg-white border-gray-100 p-2 w-full "
+              name="name"
+            />
+
             <input
               type="submit"
               className="py-2 rounded uppercase text-white bg-black text-center cursor-pointer w-full font-bold"
