b057b271a1bcae1676ae23c0346417ad1010ad7d
DaniM0613 <wwh.rj.92@gmail.com>
2024-09-27 17:36:55 -0600

    Validacion de datos con ZOD en el servidor

--- actions/create-order-actions.ts
+++ actions/create-order-actions.ts
@@ -1,6 +1,19 @@
 "use server"
 
+import { OrderSchema } from "@/src/schema"
 
-export async function createOrder() {
-    console.log('desde create-order-actions')
+
+export async function createOrder(data: unknown) {
+    const result = OrderSchema.safeParse(data)
+
+    if(!result.success){
+        return {
+            errors: result.error.issues
+        }
+    }
+    try {
+
+    }catch(error){
+        console.log(error)
+    }
 }
--- components/order/OrderSummary.tsx
+++ components/order/OrderSummary.tsx
@@ -12,21 +12,27 @@ export default function OrderSummary() {
   const order = useStore((state) => state.order)
   const total = useMemo(() => order.reduce((total, item) => total + (item.quantity * item.price), 0) ,[order])
 
-  const handleCreateOrder = (formData: FormData) => {
+  const handleCreateOrder = async (formData: FormData) => {
      const data = {
       name: formData.get('name')
      }
+
      
      const result = OrderSchema.safeParse(data)
      if(!result.success){
         result.error.issues.forEach((issue) => {
           toast.error(issue.message)
         })
+
+        return
      }
-
-     return
-
-    createOrder()
+     
+    const response = await createOrder(data)
+    if(response?.errors) {
+      response.errors.forEach((issue) => {
+        toast.error(issue.message)
+      })
+    }
   }
 
   return (
