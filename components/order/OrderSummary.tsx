4ffa4f43ea48a2b5f6a8fa705b7aec73b8f490f2
DaniM0613 <wwh.rj.92@gmail.com>
2024-09-29 22:20:22 -0600

    Evitar ordenes duplicadas

--- components/order/OrderSummary.tsx
+++ components/order/OrderSummary.tsx
@@ -10,6 +10,7 @@ import { toast } from "react-toastify"
 
 export default function OrderSummary() {
   const order = useStore((state) => state.order)
+  const clearOrder = useStore((state) => state.clearOrder)
   const total = useMemo(() => order.reduce((total, item) => total + (item.quantity * item.price), 0) ,[order])
 
   const handleCreateOrder = async (formData: FormData) => {
@@ -36,6 +37,8 @@ export default function OrderSummary() {
         toast.error(issue.message)
       })
     }
+    toast.success('Pedido realizado correctamente')
+    clearOrder()
   }
 
   return (
